6f87faf4f14adb75a4e085638a3a65794f130191
Josh Raiborde <joshsrai@yahoo.com>
2020-07-30 23:00:40 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,12 +4,18 @@
 #   "Amanda" => 27
 # }
 
-
-
-
-
-
 def happy_birthday(birthday_kids)
+  birthday_kids.each { |kids_name, age|
+  puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  }  
+  
+  # add your code snippet here!
+end
+
+
+
+
+def age_appropriate_birthday(birthday_kids)
 birthday_kids.collect do |kids_name, age|  
     if age <= 12
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
